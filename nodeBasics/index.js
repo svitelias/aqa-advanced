53ef8dcf866f045efbe8b928f3f675519b6e471d
Svitlana Elias <svitlana.elias@gmail.com>
2026-05-18 12:02:48 +0300

    feat: add node basics with chalk

---
+++ nodeBasics/index.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+console.log(chalk.hex('#FF69B4')('Pink is my preferred color'));
+console.log(chalk.cyan.bold('I Am seriously!'));
+console.log(chalk.green('Now I can use chalk!'));
