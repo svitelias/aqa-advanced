97dc8fe2fc21d1052ccf16da3c4d1ee52a676a78
Svitlana Elias <svitlana.elias@gmail.com>
2026-06-28 20:34:54 +0300

    feat: add js objects tasks

---
+++ jsObjects/task2.js
@@ -0,0 +1,10 @@
+const book = {
+  title: 'The Great Gatsby',
+  author: 'F. Scott Fitzgerald',
+  year: '1925'
+};
+
+const { title, author } = book;
+
+console.log(title);
+console.log(author);
---
+++ jsObjects/task3.js
@@ -0,0 +1,15 @@
+const car1 = {
+  brand: 'Toyota',
+  model: 'Camry',
+  year: 2020
+};
+
+const car2 = {
+  brand: 'Honda',
+  model: 'Civic',
+  owner: 'John'
+};
+
+const car3 = { ...car1, ...car2 };
+
+console.log(car3);
